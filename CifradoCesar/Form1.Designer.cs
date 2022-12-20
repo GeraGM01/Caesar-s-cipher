c6cd952b76b78a9add4b31c7597c954f33ce3cb2
Gera <Gera@DESKTOP-DEMA5FH>
2022-12-19 19:41:05 -0600

    Agregar archivos de proyecto.

---
+++ CifradoCesar/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace CifradoCesar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPalabra = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCifrada = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.txtCifrada);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtPalabra);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtC);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(297, 186);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cifrado Monoalfabetico";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "C =";
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(49, 31);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(129, 20);
+            this.txtC.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Palabra:";
+            // 
+            // txtPalabra
+            // 
+            this.txtPalabra.Location = new System.Drawing.Point(49, 64);
+            this.txtPalabra.Name = "txtPalabra";
+            this.txtPalabra.Size = new System.Drawing.Size(129, 20);
+            this.txtPalabra.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Palabra cifrada:";
+            // 
+            // txtCifrada
+            // 
+            this.txtCifrada.Location = new System.Drawing.Point(85, 134);
+            this.txtCifrada.Name = "txtCifrada";
+            this.txtCifrada.Size = new System.Drawing.Size(129, 20);
+            this.txtCifrada.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(68, 26);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Cifrar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtCifrada;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPalabra;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CifradoCesar/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CifradoCesar
+{
+    public partial class Form1 : Form
+    {
+        private List<string> alfabeto = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            inicializaAlfabeto();
+        }
+
+        public void inicializaAlfabeto()
+        {
+            for (byte a = 65; a <= 90; a++)
+            {
+                alfabeto.Add(((char)a).ToString());
+                //Console.WriteLine((char)a);
+            }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtCifrada.Text = "";
+            string mensajeCifrado = "";
+            List<string> intervaloC = new List<string>();
+            string c = txtC.Text;
+            txtPalabra.Text = txtPalabra.Text.ToUpper();
+            string mensaje = txtPalabra.Text;
+            char[] delimitador = { ',', '(', ')' };
+            string[] trozos = c.Split(delimitador);
+            for (int i = 0; i < trozos.Length; i++)
+            {
+                if (trozos[i] != "")
+                {
+                    intervaloC.Add(trozos[i]);
+                }
+            }
+            //Si es monoalfabetico y es positivo
+            if((intervaloC.ElementAt(0) == "m" || intervaloC.ElementAt(0) == "M") && Convert.ToInt32(intervaloC.ElementAt(1)) > 0)
+            {
+                //Inmediatamente hacemos una copia del alfabeto en una lista auxiliar
+                List<string> alfabetoAux = new List<string>(alfabeto);
+                //Apuntamos al final de la lista del alfabeto
+                int indexFinal = alfabeto.Count;
+                int indexInicial = indexFinal - Convert.ToInt32(intervaloC.ElementAt(1));
+                //Reemplazamos los ultimos n digitos de acuerdo al intervalo
+                int itemAlfabeto = 0;
+                for (int i = indexInicial; i < alfabeto.Count(); i++)
+                {
+                    alfabetoAux[i] = alfabeto[itemAlfabeto];
+                    //alfabetoAux.Insert(i, alfabeto[itemAlfabeto]);
+                    itemAlfabeto++;
+                }
+                //Actualizamos los valores restantes del alfabeto iniciando desde la siguiente letra que le sigue al item
+                for (int i = 0; i < indexInicial; i++)
+                {
+                    alfabetoAux[i] = alfabeto[itemAlfabeto];
+                    //alfabetoAux.Insert(i, alfabeto[itemAlfabeto]);
+                    itemAlfabeto++;
+                }
+
+                //Aqui comienza la traduccion para tener el mensaje cifrado
+
+                //buscamos el indice de la letra en nuestro alfabeto original y lo empatamos con el alfabeto que generamos 
+                foreach(char caracter in mensaje)
+                {
+                    string cAux = caracter.ToString();
+                    int indexOG = getIndexAlfabeto(cAux);
+                    string s = alfabeto.ElementAt(indexOG);
+                    if(indexOG != -1)
+                    {
+                        int indexAux = getIndexAlfabeto(s);
+                        string sAux = alfabetoAux.ElementAt(indexAux);
+                        mensajeCifrado += sAux;
+                    }
+                }
+                txtCifrada.Text = txtCifrada.Text + mensajeCifrado;
+            }
+            //Caso monoalfabetico y negativo
+            else if((intervaloC.ElementAt(0) == "m" || intervaloC.ElementAt(0) == "M") && Convert.ToInt32(intervaloC.ElementAt(1)) < 0)
+            {
+                //Inmediatamente hacemos una copia del alfabeto en una lista auxiliar
+                List<string> alfabetoAux = new List<string>(alfabeto);
+
+                //Apuntamos al inicio del alfabeto
+                int indexInicial = 0;
+                int indexFinal = Convert.ToInt32(intervaloC.ElementAt(1)) * -1;
+                int indexInicialAux = alfabeto.Count() + Convert.ToInt32(intervaloC.ElementAt(1));
+
+                //Reemplazamos los primeros n digitos de acuerdo al intervalo
+                int itemAlfabeto = indexInicialAux;
+                for (int i = indexInicial; i < indexFinal; i++)
+                {
+                    alfabetoAux[i] = alfabeto[itemAlfabeto];
+                    //alfabetoAux.Insert(i, alfabeto[itemAlfabeto]);
+                    itemAlfabeto++;
+                }
+                //Actualizamos los valores restantes del alfabeto iniciando desde la siguiente letra que le sigue al item
+                int j = 0;
+                for (int i = indexFinal; i < alfabeto.Count(); i++)
+                {
+                    alfabetoAux[i] = alfabeto[j];
+                    //alfabetoAux.Insert(i, alfabeto[itemAlfabeto]);
+                    j++;
+                }
+
+                //Aqui comienza la traduccion para tener el mensaje cifrado
+
+                //buscamos el indice de la letra en nuestro alfabeto original y lo empatamos con el alfabeto que generamos 
+                foreach (char caracter in mensaje)
+                {
+                    string cAux = caracter.ToString();
+                    int indexOG = getIndexAlfabeto(cAux);
+                    string s = alfabeto.ElementAt(indexOG);
+                    if (indexOG != -1)
+                    {
+                        int indexAux = getIndexAlfabeto(s);
+                        string sAux = alfabetoAux.ElementAt(indexAux);
+                        mensajeCifrado += sAux;
+                    }
+                }
+                txtCifrada.Text = txtCifrada.Text + mensajeCifrado;
+            }
+        }
+
+        public int getIndexAlfabeto(string letra)
+        {
+            for (int i = 0; i < alfabeto.Count(); i++)
+            {
+                string s = alfabeto[i];
+                if(letra == s)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+    }
+}
